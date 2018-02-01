f3712eb222f1205a48f24175467685d20d1a7872
shansmann <sebastian.hansmann@googlemail.com>
2018-02-01 16:48:45 +0100

    added preview files

--- data/create_data.py
+++ data/create_data.py
@@ -6,9 +6,5 @@ path = '/Users/sebastianhansmann/Documents/Code/TU/mt/data/semeval/test/'
 semeval = DataCollection('semeval')
 semeval.parse_input_data(path)
 
-#semeval_processor = Processor(semeval)
-#semeval_processor.create_conll_format()
-
-
 semeval_processor_ann = AnnProcessor(semeval, path)
 semeval_processor_ann.create_conll_format('/Users/sebastianhansmann/Documents/Code/TU/mt/data/semeval/test/conll.tsv')
