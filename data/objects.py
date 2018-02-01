ec30a96799084922062302a229b5903fde9024ab
shansmann <sebastian.hansmann@googlemail.com>
2018-02-01 16:42:44 +0100

    added data processing for plain text and brat annotated text

---
+++ data/objects.py
@@ -0,0 +1,68 @@
+import glob
+import os
+from os.path import basename
+from nltk.tokenize import word_tokenize
+from nltk.tokenize.punkt import PunktSentenceTokenizer
+
+
+class Word:
+	def __init__(self, word, start, end, entity = 'O'):
+		self.word = word
+		self.start = int(start)
+		self.end = int(end)
+		self.entity = entity
+
+	def __repr__(self):
+		return str(self.__dict__)
+
+
+class Annotation:
+	def __init__(self, surface, start, end, entity):
+		self.word = surface
+		self.start = start
+		self.end = end
+		self.entity = entity
+
+	def __repr__(self):
+		return str(self.__dict__)
+
+
+class Document:
+	def __init__(self, id, sentence_breaks, tokens, plain_text):
+		self.id = id
+		self.sentence_breaks = sentence_breaks
+		self.tokens = tokens
+		self.plain = plain_text
+
+
+class DataCollection:
+	def __init__(self, name):
+		self.name = name
+		self.documents = []
+
+	def _spans(self, txt):
+		tokens = word_tokenize(txt)
+		offset = 0
+		for token in tokens:
+			offset = txt.find(token, offset)
+			yield token, offset, offset + len(token)
+			offset += len(token)
+
+	def parse_input_data(self, path):
+		docs = []
+		for filename in glob.glob(os.path.join(path, '*.txt')):
+			file = open(filename, "r")
+			base_name = basename(filename).split('.')[0]
+			file_content = file.read()
+			words = []
+			sentence_breaks = []
+			for token in self._spans(file_content):
+				words.append(Word(token[0], token[1], token[2]))
+			for start, end in PunktSentenceTokenizer().span_tokenize(file_content):
+				sentence_breaks.append(end)
+			doc = Document(base_name, sentence_breaks, words, file_content)
+			docs.append(doc)
+
+			file.close()
+
+		self.documents = docs
---
+++ data/processor.py
@@ -0,0 +1,73 @@
+from objects import Word, Annotation, Document, DataCollection
+
+class Processor:
+	def __init__(self, DataCollection):
+		self.data_collection = DataCollection
+
+	def create_conll_format(self):
+		for document in self.data_collection.documents:
+			for token in document.tokens:
+				if (token.start-1) in (document.sentence_breaks):
+					print()
+				else:
+					print(token.entity, token.start, token.end, token.word)
+
+class AnnProcessor(Processor):
+	def __init__(self, DataCollection, annotation_path):
+		self.data_collection = DataCollection
+		self.annotation_path = annotation_path
+
+	def load_ann_file(self, path):
+		annos = []
+		f_anno = open(path, "rU")
+		for line in f_anno:
+			anno_inst = line.strip("\n").split("\t")
+			if len(anno_inst) == 3:
+				anno_inst1 = anno_inst[1].split(" ")
+				if len(anno_inst1) == 3:
+					keytype, start, end = anno_inst1
+				else:
+					keytype, start, _, end = anno_inst1
+				if not keytype.endswith("-of"):
+					surface = anno_inst[2]
+					annos.append(Annotation(surface, int(start), int(end), keytype))
+		f_anno.close()
+		return annos
+
+	def create_conll_format(self, outpath=''):
+		for document in self.data_collection.documents:
+			# load ann file
+			ann_path = str(self.annotation_path + document.id + '.ann')
+			annos = self.load_ann_file(ann_path)
+			if outpath:
+				with open(outpath, "w") as record_file:
+					for token in document.tokens:
+						# check annotations
+						#TODO: check for annotation overlap!
+						for anno in annos:
+							if token.start >= anno.start and token.end <= anno.end:
+								token.entity = anno.entity
+						if (token.start - 1) in (document.sentence_breaks):
+							record_file.write('\n')
+						else:
+							line = "{}	{}	{}	{}\n".format(token.entity, token.start, token.end, token.word)
+							record_file.write(line)
+			else:
+				for token in document.tokens:
+					# check annotations
+					#TODO: check for annotation overlap!
+					for anno in annos:
+						if token.start >= anno.start and token.end <= anno.end:
+							token.entity = anno.entity
+					if (token.start - 1) in (document.sentence_breaks):
+						print()
+					else:
+						print(token.entity, token.start, token.end, token.word)
+
+
+class HeuristikProcessor(Processor):
+	def __init__(self):
+		pass
+
+	def create_conll_format(self):
+		pass
