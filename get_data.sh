af8c07392387949ada5ca8b82225ab09c4273892
shansmann <sebastian.hansmann@googlemail.com>
2018-06-12 12:51:03 +0200

    glove already on server

--- get_data.sh
+++ get_data.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-wget http://nlp.stanford.edu/data/glove.840B.300d.zip --directory-prefix=./ner/
-unzip glove.840B.300d.zip
-rm glove.840B.300d.zip
+#wget http://nlp.stanford.edu/data/glove.840B.300d.zip --directory-prefix=./ner/
+#unzip glove.840B.300d.zip
+#rm glove.840B.300d.zip
 
 wget --no-check-certificate --no-proxy 'https://s3-eu-west-1.amazonaws.com/sensor-corpus-auto/train.txt' --directory-prefix=./data/sensor_corpus/
 wget --no-check-certificate --no-proxy 'https://s3-eu-west-1.amazonaws.com/sensor-corpus-auto/dev.txt' --directory-prefix=./data/sensor_corpus/
