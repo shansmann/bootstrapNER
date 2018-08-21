10ebfc2cdf86b455908ec872b7ce660d0b102c34
shansmann <sebastian.hansmann@googlemail.com>
2018-08-21 16:58:47 +0200

    updated log folder usage

--- run_product.sh
+++ run_product.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=4 python Train_NER_Product.py > log_product.txt
-CUDA_VISIBLE_DEVICES=4 python Train_NER_Sensor_Sample.py False > log_sensor_10_false.txt
-CUDA_VISIBLE_DEVICES=4 python Train_NER_Sensor_Sample.py dropout > log_sensor_10_dropout.txt
-CUDA_VISIBLE_DEVICES=4 python Train_NER_Sensor_Sample.py trace > log_sensor_10_trace.txt
-CUDA_VISIBLE_DEVICES=4 python Train_NER_Sensor_Sample.py fix_train /mnt/hdd/experiments/shansmann/bootstrapNER/data/weights_product.txt > log_sensor_10_fix_train.txt
-CUDA_VISIBLE_DEVICES=4 python Train_NER_Sensor_Sample.py fix_fix /mnt/hdd/experiments/shansmann/bootstrapNER/data/weights_product.txt > log_sensor_10_fix_fix.txt
+CUDA_VISIBLE_DEVICES=4 python ner/Train_NER_Product.py > ner/logs/product/log_product.txt
+CUDA_VISIBLE_DEVICES=4 python ner/Train_NER_Sensor_Sample.py False > ner/logs/product/log_sensor_10_false.txt
+CUDA_VISIBLE_DEVICES=4 python ner/Train_NER_Sensor_Sample.py dropout > ner/logs/product/log_sensor_10_dropout.txt
+CUDA_VISIBLE_DEVICES=4 python ner/Train_NER_Sensor_Sample.py trace > ner/logs/product/log_sensor_10_trace.txt
+CUDA_VISIBLE_DEVICES=4 python ner/Train_NER_Sensor_Sample.py fix_train /mnt/hdd/experiments/shansmann/bootstrapNER/data/weights_product.txt > ner/logs/product/log_sensor_10_fix_train.txt
+CUDA_VISIBLE_DEVICES=4 python ner/Train_NER_Sensor_Sample.py fix_fix /mnt/hdd/experiments/shansmann/bootstrapNER/data/weights_product.txt > ner/logs/product/log_sensor_10_fix_fix.txt
--- run_semeval.sh
+++ run_semeval.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=5 python Train_NER_Semeval.py > log_semeval.txt
-CUDA_VISIBLE_DEVICES=5 python Train_NER_Science.py False > log_science_false.txt
-CUDA_VISIBLE_DEVICES=5 python Train_NER_Science.py dropout > log_science_dropout.txt
-CUDA_VISIBLE_DEVICES=5 python Train_NER_Science.py trace > log_science_trace.txt
-CUDA_VISIBLE_DEVICES=5 python Train_NER_Science.py fix_train /mnt/hdd/experiments/shansmann/bootstrapNER/data/weights_sem.txt > log_science_fix_train.txt
-CUDA_VISIBLE_DEVICES=5 python Train_NER_Science.py fix_fix /mnt/hdd/experiments/shansmann/bootstrapNER/data/weights_sem.txt > log_science_fix_fix.txt
+CUDA_VISIBLE_DEVICES=5 python ner/Train_NER_Semeval.py > ner/logs/semeval/log_semeval.txt
+CUDA_VISIBLE_DEVICES=5 python ner/Train_NER_Science.py False > ner/logs/semeval/log_science_false.txt
+CUDA_VISIBLE_DEVICES=5 python ner/Train_NER_Science.py dropout > ner/logs/semeval/log_science_dropout.txt
+CUDA_VISIBLE_DEVICES=5 python ner/Train_NER_Science.py trace > ner/logs/semeval/log_science_trace.txt
+CUDA_VISIBLE_DEVICES=5 python ner/Train_NER_Science.py fix_train /mnt/hdd/experiments/shansmann/bootstrapNER/data/weights_sem.txt > ner/logs/semeval/log_science_fix_train.txt
+CUDA_VISIBLE_DEVICES=5 python ner/Train_NER_Science.py fix_fix /mnt/hdd/experiments/shansmann/bootstrapNER/data/weights_sem.txt > ner/logs/semeval/log_science_fix_fix.txt
 
 
 #!/usr/bin/env bash
